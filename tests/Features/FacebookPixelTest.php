355fbd86bdfa34412b3f6cc5c815cb3246d2dc31
Omar Herri <herri.omar@gmail.com>
2021-03-23 22:34:35 +0100

    wip

--- tests/Features/FacebookPixelTest.php
+++ tests/Features/FacebookPixelTest.php
@@ -2,7 +2,6 @@
 
 namespace Combindma\FacebookPixel\Tests\Features;
 
-use Combindma\FacebookPixel\FacebookPixel;
 use Combindma\FacebookPixel\Tests\TestCase;
 
 class FacebookPixelTest extends TestCase
@@ -15,9 +14,9 @@ class FacebookPixelTest extends TestCase
 
     public function test_bodyContent()
     {
-        FacebookPixel::addEvent('viewContent', ['content_type' => 'product',]);
+        $this->facebookPixel->createEvent('viewContent', ['content_type' => 'product',]);
         $this->assertEquals("<script>fbq('track', 'viewContent', {\"content_type\":\"product\"});</script>", $this->facebookPixel->bodyContent());
-        FacebookPixel::addEvent('viewContent', ['content_type' => 'product',]);
+        $this->facebookPixel->createEvent('viewContent', ['content_type' => 'product',]);
         $this->assertEquals("<script>fbq('track', 'viewContent', {\"content_type\":\"product\"});</script>", facebookPixelBody());
     }
 }
